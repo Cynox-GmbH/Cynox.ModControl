f76ff72db48342fde23abab92b6a6bf3ad45305e
Marcus Freese <936992+RevZero@users.noreply.github.com>
2023-04-13 13:20:04 +0200

    - Include package license information. - Added example to test project.

---
+++ ModControl.Tests/Examples.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Cynox.ModControl.Connections;
+using Cynox.ModControl.Devices;
+using Cynox.ModControl.Protocol.Commands;
+using NUnit.Framework;
+
+namespace ModControlTests
+{
+    internal class Examples
+    {
+        [Test]
+        public void QuickStart()
+        {
+            // Create default device
+            var device = new ModControlDevice {
+                Address = 1,
+            };
+
+            // Establish connection
+            bool success = device.Connect(new TcpConnection("192.168.6.155", 1470));
+
+            if (success)
+            {
+                var setOutputResponse = device.SetOutput(0, true);
+
+                // Check if request was successful
+                if (setOutputResponse.Error != ResponseError.None)
+                {
+                    Debug.WriteLine($"Request failed: {setOutputResponse.Error}");
+                }
+
+                var getCounterResponse = device.GetCounter(2);
+
+                // Check if request was successful
+                if (getCounterResponse.Error != ResponseError.None)
+                {
+                    Debug.WriteLine($"Request failed: {getCounterResponse.Error}");
+                }
+                else
+                {
+                    // Log counter value
+                    Debug.WriteLine($"Current counter value for channel {getCounterResponse.Channel} = {getCounterResponse.Value}");
+                }
+            }
+
+            // Disconnect
+            device.Disconnect();
+        }
+    }
+}
